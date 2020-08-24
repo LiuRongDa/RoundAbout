3b720204986ab8a2391144c1277580210126c25c
lrd <1404292909@qq.com>
2020-08-24 14:27:29 +0800

    dao

--- src/main/java/com/aaa/dao/TbUserMapper.java
+++ src/main/java/com/aaa/dao/TbUserMapper.java
@@ -1,7 +1,10 @@
 package com.aaa.dao;
 
 import com.aaa.entity.TbUser;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.MyMapper;
 
 public interface TbUserMapper extends MyMapper<TbUser> {
+    @Select("select * from tb_user where userName=#{userName}")
+    TbUser findName(String userName);
 }
