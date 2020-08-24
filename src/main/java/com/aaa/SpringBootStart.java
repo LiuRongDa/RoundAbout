c2a7c64a36e2e3e7848f802e825be86da21fbbc2
lrd <1404292909@qq.com>
2020-08-24 10:54:59 +0800

    SpringBootStart

---
+++ src/main/java/com/aaa/SpringBootStart.java
@@ -0,0 +1,18 @@
+package com.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @description:
+ * @author: LRD
+ * @time: 2020/8/23 0:23
+ */
+@SpringBootApplication
+@MapperScan("com.aaa.dao")
+public class SpringBootStart {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStart.class);
+    }
+}
